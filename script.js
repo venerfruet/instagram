d4481d6476674b03b8715c130e7daf1afc0f3c10
venerfruet <venerfruet@gmail.com>
2022-07-06 12:50:57 -0300

    update

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 const BASE_URL='https://graph.instagram.com/9943873833?fields=id,media_type,media_url,username,timestamp&access_token=IGQVJVdEdrUzgxRE9OVHJTaFdTVXJYLWJsVUg3TW1LVzFIUjlyeUJta1lFdUU2VXd4cFBWZAXdENDdHM3hoUVlfTFBtb2JOYmdSZAnl3c1czZAUZA1SHhfNHlyUTBkeG1nQVh5aWhPbFpCalBRWDZAQb0oxWAZDZD';
 const RESULT_DIV=document.querySelector('#result');
 
-const getInsta=async ()=>{
+const getInsta=()=>{
   try{
-
-     const data=await fetch(BASE_URL)
-     .then(res=>res.json())
-     .catch(e=>console.log(e))
+     const data=fetch(BASE_URL)
+      .then(res=>res.json())
+      .catch(e=>console.log(e))
 
      console.log(data);
 
