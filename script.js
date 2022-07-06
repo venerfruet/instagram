f2fd83dc378f23863ef7957c3da83ebe20c3881e
venerfruet <venerfruet@gmail.com>
2022-07-06 12:18:00 -0300

    update script

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
+let url='https://graph.instagram.com/Bs0KTU5A08T?fields=id,media_type,media_url,username,timestamp&access_token=IGQVJVdEdrUzgxRE9OVHJTaFdTVXJYLWJsVUg3TW1LVzFIUjlyeUJta1lFdUU2VXd4cFBWZAXdENDdHM3hoUVlfTFBtb2JOYmdSZAnl3c1czZAUZA1SHhfNHlyUTBkeG1nQVh5aWhPbFpCalBRWDZAQb0oxWAZDZD';
 let result=document.querySelector('#result');
 result.innerHTML='teste';
