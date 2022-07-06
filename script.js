63bb7ad97d80211ccafd589f61f620b35dd9ca6c
venerfruet <venerfruet@gmail.com>
2022-07-06 12:37:50 -0300

    update script

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-let url='https://graph.instagram.com/Bs0KTU5A08T?fields=id,media_type,media_url,username,timestamp&access_token=IGQVJVdEdrUzgxRE9OVHJTaFdTVXJYLWJsVUg3TW1LVzFIUjlyeUJta1lFdUU2VXd4cFBWZAXdENDdHM3hoUVlfTFBtb2JOYmdSZAnl3c1czZAUZA1SHhfNHlyUTBkeG1nQVh5aWhPbFpCalBRWDZAQb0oxWAZDZD';
-let result=document.querySelector('#result');
-result.innerHTML='teste';
+const BASE_URL='https://graph.instagram.com/9943873833?fields=id,media_type,media_url,username,timestamp&access_token=IGQVJVdEdrUzgxRE9OVHJTaFdTVXJYLWJsVUg3TW1LVzFIUjlyeUJta1lFdUU2VXd4cFBWZAXdENDdHM3hoUVlfTFBtb2JOYmdSZAnl3c1czZAUZA1SHhfNHlyUTBkeG1nQVh5aWhPbFpCalBRWDZAQb0oxWAZDZD';
+const RESULT_DIV=document.querySelector('#result');
+
+RESULT_DIV.innerHTML=await getInsta();
+
+
+const getInsta=async ()=>{
+  try{
+
+     const data=await fetch(BASE_URL)
+     .then(res=>res.json())
+     .catch(e=>console.log(e))
+
+     return data.webpurl;
+
+  }catch(e){
+     console.log(e.message);
+  }
+  
+}
